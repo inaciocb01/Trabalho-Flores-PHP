3c94784258cad7dfd8314721e3ac250d78217d60
inaciocb01 <inacbarbosa07@gmail.com>
2026-09-09 19:25:04 -0300

    Add files via upload

---
+++ code03.php
@@ -0,0 +1,13 @@
+        <?php
+        $nota = 0.8;
+
+        if ($nota >= 90) {
+            echo "Nota A";
+        } elseif ($nota >= 80) {
+            echo "Nota B";
+        } elseif ($nota >= 70) {
+            echo "Nota C";
+        } else {
+            echo "Nota abaixo de C";
+        }
+        ?>
