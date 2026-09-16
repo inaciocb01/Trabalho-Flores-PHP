a8aa355f0a17c71e49c21be052d9d2f4e06ffe72
alchart33-cyber <alchart33@gmail.com>
2026-09-16 20:12:10 -0300

    Add files via upload

---
+++ exercicio55.php
@@ -0,0 +1,43 @@
+<?php
+/**************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: Flores
+Turma: ESOFT-2
+Componentes:
+RA 26008762-2 - Caik dos Santos Cassiano       
+RA 26007934-2 - Inácio Correa Barbosa           
+RA 26008105-2 - Jonathan Manuel Rodríguez Barba 
+RA 26008853-2 - Kaylane Rosa Domingos           
+RA 26003569-2 - Lucas Eduardo Ferreira Branco   
+RA 26014449-2 - Vitor Hugo Carvalho Stenger     
+Data: 19 de Setembro de 2026
+Descritivo: Programa que le um número do 1 até o 12 e fala o mês que é.
+***************************/
+
+
+
+ $numero = (int) readline("Digite um número de 1 a 12: ");
+
+$meses = [
+    1 => "Janeiro",
+    2 => "Fevereiro",
+    3 => "Março",
+    4 => "Abril",
+    5 => "Maio",
+    6 => "Junho",
+    7 => "Julho",
+    8 => "Agosto",
+    9 => "Setembro",
+    10 => "Outubro",
+    11 => "Novembro",
+    12 => "Dezembro"
+];
+
+if ($numero >= 1 && $numero <= 12) {
+    echo $meses[$numero];
+} else {
+    echo "Número inválido. Digite um número entre 1 e 12.";
+}
+
+?>
