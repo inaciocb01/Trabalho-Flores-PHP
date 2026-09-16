93d06aae9a4c454e4691d64be4c94e0ad42fd80d
alchart33-cyber <alchart33@gmail.com>
2026-09-16 19:59:52 -0300

    Add files via upload

--- 57.php
+++ 57.php
@@ -5,15 +5,16 @@ Disciplina: Linguagem e Técnicas de Programacão
 Professor: Flores
 Turma: ESOFT-2
 Componentes:
-RA 26003569-2 - Lucas Eduardo Ferreira Branco   = 01 ao 10 Exercício
-RA 26008853-2 - Kaylane Rosa Domingos           = 11 ao 20 Exercício
-RA 26007934-2 - Inácio Correa Barbosa           = 21 ao 30 Exercício
-RA 26008762-2 - Caik dos Santos Cassiano        = 31 ao 40 Exercício
-RA 26014449-2 - Vitor Hugo Carvalho Stenger     = 41 ao 50 Exercício
-RA 26008105-2 - Jonathan Manuel Rodríguez Barba = 51 ao 60 Exercício
+RA 26008762-2 - Caik dos Santos Cassiano       
+RA 26007934-2 - Inácio Correa Barbosa           
+RA 26008105-2 - Jonathan Manuel Rodríguez Barba 
+RA 26008853-2 - Kaylane Rosa Domingos           
+RA 26003569-2 - Lucas Eduardo Ferreira Branco   
+RA 26014449-2 - Vitor Hugo Carvalho Stenger     
 Data: 19 de Setembro de 2026
-Descritivo: Programa que le o dia , o mês e o ano e define se a data é valida .
+Descritivo:Programa que lê o dia , o mês e o ano e define se a data é valida .
 ***************************/
+
 
 $dia = (int) readline("Digite o dia: ");
 $mes = (int) readline("Digite o mês: ");
